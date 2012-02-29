71a6b2cc77b5ca5e40ba4a91177f840b08f606fa
Christophe Van Gysel <chris@stophr.be>
2012-02-29 11:48:15 +0100

    Created dummy User stub interface and two stub implementations for authentication plugin

--- drupalgitdaemon/__init__.py
+++ drupalgitdaemon/__init__.py
@@ -1,4 +1,17 @@
+from zope.interface.declarations import implements
 from zope.interface.interface import Interface
 
-class IDrupalMeta(Interface):
-    """"""
+class IUser(Interface):
+    """ """
+
+    def mayRead(self, label = None):
+        """ """
+
+    def mayWrite(self, label = None):
+        """ """
+
+class User(object):
+    implements(IUser)
+
+class AnonymousUser(object):
+    implements(IUser)
