bf367f507d2898d01ccc6b7d10f901ef68866330
Christophe Van Gysel <chris@stophr.be>
2012-04-26 18:25:42 +0200

    Moved Drupal.org implementation to gitexd-drupalorg

--- drupalgitdaemon/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-from gitexd.tests import _createDefaultConfigFile
-
-def _createDrupalAuthConfigFile(repoPath = '', allowAnon = False):
-    defaults = {
-        "allowAnonymous": allowAnon,
-        "authServiceProtocol": "dummy"
-    }
-
-    return _createDefaultConfigFile(repoPath, defaults)
